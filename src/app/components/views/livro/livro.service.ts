580605bf2a7ac55b95ce0077afb06a255d22ee4a
Neri dos Santos Lima Junior <nerijunior@detransc.sc.gov.br>
2023-05-16 13:56:27 -0300

    Implementado all livros por categoria

---
+++ src/app/components/views/livro/livro.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Livro } from './livro.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LivroService {
+
+  baseUrl: String = environment.baseUrl;
+
+
+  constructor(private http: HttpClient) { }
+
+  findAllByCategoria(id_cat: String): Observable<Livro[]> {
+    const url = `${this.baseUrl}/livros?categoria=${id_cat}`
+    return this.http.get<Livro[]>(url)
+
+  }
+}

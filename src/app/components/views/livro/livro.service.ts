5a2c37aebf65903517a85c6bd4cf4d7c2e52e003
Neri dos Santos Lima Junior <nerijunior@detransc.sc.gov.br>
2023-05-16 17:46:35 -0300

    implementado livro delete

--- src/app/components/views/livro/livro.service.ts
+++ src/app/components/views/livro/livro.service.ts
@@ -38,6 +38,11 @@ export class LivroService {
     return this.http.post<Livro>(url, livro);
   }
 
+  delete(id: String):Observable<void>{
+    const url = `${this.baseUrl}/livros/${id}`
+    return this.http.delete<void>(url)
+  }
+
   mensagem(str: String): void {
     this._snake.open(`${str}`, 'OK', {
       horizontalPosition: 'end',
